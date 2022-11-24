f36f9d0d1a3d6650ef71028206c373e3d1b2c145
Simon <simon@appolo.fr>
2022-11-24 17:01:11 +0100

    Nettoyage + Ajout de scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import React, { useState } from 'react';
-import './App.css';
-import {
-    closestCenter,
-    DndContext, DragEndEvent,
-    KeyboardSensor,
-    PointerSensor,
-    useSensor,
-    useSensors
-} from "@dnd-kit/core";
-import {
-    arrayMove,
-    SortableContext,
-    sortableKeyboardCoordinates,
-    verticalListSortingStrategy
-} from "@dnd-kit/sortable";
-import { SortableItem } from "./components/SortableItem";
+import './App.scss';
 import { blocks } from './components/blocks/definition';
 import { BlockType } from "./components/BlockType";
 
 function App() {
-    const [items, setItems] = useState([1, 2, 3]);
-
     const [items2, setItems2] = useState([
         {
             type: "TextInput",
@@ -69,7 +52,7 @@ function App() {
                     label: "Select",
                     tooltip: "",
                     multiple: false,
-                    options: [{ label: "test", value: "testt"}],
+                    options: [{label: "test", value: "testt"}],
                     required: true,
                 },
             ]
@@ -91,28 +74,6 @@ function App() {
         }
     ]);
 
-    const sensors = useSensors(
-        useSensor(PointerSensor),
-        useSensor(KeyboardSensor, {
-            coordinateGetter: sortableKeyboardCoordinates,
-        })
-    );
-
-    const handleDragEnd = (event: DragEndEvent) => {
-        const {active, over} = event;
-
-        if (active.id !== over?.id) {
-            setItems((items) => {
-                // @ts-ignore
-                const oldIndex = items.indexOf(active.id);
-                // @ts-ignore
-                const newIndex = items.indexOf(over?.id);
-
-                return arrayMove(items, oldIndex, newIndex);
-            });
-        }
-    }
-
     const editItem = (item: any, key: string, value: any) => {
         item[key] = value;
         setItems2([...items2]);
@@ -120,20 +81,13 @@ function App() {
 
     return (
         <>
-            <DndContext
-                sensors={sensors}
-                collisionDetection={closestCenter}
-                onDragEnd={handleDragEnd}
-            >
-                <SortableContext
-                    items={items}
-                    strategy={verticalListSortingStrategy}
-                >
-                    {items.map(id => <SortableItem key={id} id={id} />)}
-                </SortableContext>
-            </DndContext>
-            {items2.map((item, i) => React.createElement(blocks[item.type].component,{ key: i, ...item, editItem: (key: string, value: any) => editItem(item, key, value) }))}
-            {Object.entries(blocks).map(([key, block], i) => <BlockType block={block} key={i} />)}
+            {items2.map((item, i) =>
+                React.createElement(blocks[item.type].component, {
+                    key: i, ...item,
+                    editItem: (key: string, value: any) => editItem(item, key, value)
+                }))
+            }
+            {Object.entries(blocks).map(([key, block], i) => <BlockType block={block} key={i}/>)}
 
             <p>{JSON.stringify(items2)}</p>
         </>
